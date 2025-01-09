5be751750e9cf9b346da54ee0c02712099f3e8dd
Harsh Sharma <sharmahs017@gmail.com>
2025-01-09 13:33:22 +0530

    Implement user data fetching and middleware for authentication

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
@@ -7,6 +8,7 @@ import {useRouter} from "next/navigation";
 export default function ProfilePage() {
 
     const router = useRouter();
+    const [data, setData] = React.useState("nothing");
     const logout = async () => {
         try {
             await axios.get('/api/users/logout')
@@ -16,16 +18,33 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        setData(res.data.data._id)
+
+    }
+
     return (
         <div className=" text-center mt-5 flex flex-col items-center min-h-screen ">
             <h1>Profile</h1>
             <p>Profile Page</p>
+            <h2>{data === 'nothing' ? "Nothing" : 
+                <Link href={`/profile/${data}`}>
+                    {data}
+                </Link>
+                }</h2>
 
             <hr />
+
             <button
             onClick={logout}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
             >Log Out</button>
+
+            <button
+            onClick={getUserDetails}
+            className="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            >Get User Detail</button>
         </div>
     );
 }
